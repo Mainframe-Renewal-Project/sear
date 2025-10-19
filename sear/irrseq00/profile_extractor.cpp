3a41ce3f735851ba4b71ba1db63feff4beaba82e
Emma Skovgaard <64806882+EmmasBox@users.noreply.github.com>
2025-10-19 13:29:40 +0200

    test

--- sear/irrseq00/profile_extractor.cpp
+++ sear/irrseq00/profile_extractor.cpp
@@ -77,6 +77,8 @@ void ProfileExtractor::extract(SecurityRequest &request) {
     request.setRawRequestPointer(ProfileExtractor::cloneBuffer(
         reinterpret_cast<char *>(p_arg_area), request.getRawRequestLength()));
 
+    Logger::getInstance().debug(p_arg_area);
+
     // Call R_Admin
     Logger::getInstance().debug("Calling IRRSEQ00 ...");
     rc = callRadmin(reinterpret_cast<char *__ptr32>(&p_arg_area->arg_pointers));
