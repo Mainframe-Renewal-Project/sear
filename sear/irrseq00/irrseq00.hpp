e553e518a789cf44f1aba31bf26448f021e060a0
Emma Skovgaard <64806882+EmmasBox@users.noreply.github.com>
2025-10-19 13:50:06 +0200

    fixed some bugs

--- sear/irrseq00/irrseq00.hpp
+++ sear/irrseq00/irrseq00.hpp
@@ -295,7 +295,7 @@ typedef struct {
   char racf_subsystem_userid[8];
   char reserved_space[52];
   racf_rrsf_node_definitions_t node_definitions;
-} racf_rrsf_extract_parms_t;
+} racf_rrsf_extract_results_t;
 
 typedef struct {
   char RACF_work_area[1024];
@@ -328,7 +328,6 @@ typedef struct {
   uint32_t *__ptr32 p_RACF_rsn;
   // extract function to perform
   uint8_t *__ptr32 p_function_code;
-  racf_rrsf_extract_parms_t *__ptr32 p_racf_options_extract_parms;
   char *__ptr32 p_profile_name;
   // Result area for the service
   uint32_t *__ptr32 p_ACEE;
--- sear/irrseq00/profile_extractor.cpp
+++ sear/irrseq00/profile_extractor.cpp
@@ -276,7 +276,11 @@ void ProfileExtractor::extract(SecurityRequest &request) {
     const generic_extract_parms_results_t *p_generic_result =
         reinterpret_cast<const generic_extract_parms_results_t *>(p_raw_result);
     raw_result_length = ntohl(p_generic_result->result_buffer_length);
-  } else {
+  } else if (request.getAdminType() == "racf-rrsf") {
+    const racf_rrsf_extract_results_t *p_rrsf_result =
+        reinterpret_cast<const racf_rrsf_extract_results_t *>(p_raw_result);
+    raw_result_length = ntohl(p_rrsf_result->result_buffer_length);    
+  } else if (request.getAdminType() == "racf-options") {
     const racf_options_extract_results_t *p_setropts_result =
         reinterpret_cast<const racf_options_extract_results_t *>(p_raw_result);
     raw_result_length = ntohl(p_setropts_result->result_buffer_length);
