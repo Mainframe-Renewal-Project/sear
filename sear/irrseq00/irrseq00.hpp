bec6ce34212649a6bcd043a57083c7d390e0ca8e
Emma Skovgaard <64806882+EmmasBox@users.noreply.github.com>
2025-10-19 10:15:30 +0200

    fixed bug

--- sear/irrseq00/irrseq00.hpp
+++ sear/irrseq00/irrseq00.hpp
@@ -272,8 +272,7 @@ typedef struct {
 typedef struct {
   char eyecatcher[4];
   uint8_t subpool_buffer_length[1];
-  uint32_t output_buffer_length[3];
-  uint32_t output_buffer_length[4];
+  uint32_t total_length_output_buffer[3];
   char subsystem_prefix[8];
   uint32_t rrsf_node_index[4];
   uint8_t automatic_command_redirection[1];
