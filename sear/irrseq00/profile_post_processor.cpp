efbc69eb5bf2d229296a2bc7a980911474b1ba80
Emma Skovgaard <64806882+EmmasBox@users.noreply.github.com>
2025-11-02 21:30:57 +0100

    test

--- sear/irrseq00/profile_post_processor.cpp
+++ sear/irrseq00/profile_post_processor.cpp
@@ -222,7 +222,7 @@ void ProfilePostProcessor::postProcessRACFRRSF(SecurityRequest &request) {
       reinterpret_cast<const racf_rrsf_extract_results_t *>(
           p_profile + sizeof(racf_rrsf_extract_results_t));
 
-  profile["profile"]["rrsf:base"]["base:subsystem_name"] = toUTF8(rrsf_extract_result->racf_subsystem_name);
+  profile["profile"]["rrsf:base"]["base:subsystem_name"] = toUTF8(std::string(rrsf_extract_result->racf_subsystem_name));
   
   request.setIntermediateResultJSON(profile);
 }
