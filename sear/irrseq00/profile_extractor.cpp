ed2194b2f9fdb07453609cb5b624c8a4e1f31e51
Emma Skovgaard <64806882+EmmasBox@users.noreply.github.com>
2025-11-02 19:57:15 +0100

    test

--- sear/irrseq00/profile_extractor.cpp
+++ sear/irrseq00/profile_extractor.cpp
@@ -280,7 +280,7 @@ void ProfileExtractor::extract(SecurityRequest &request) {
   } else if (request.getAdminType() == "racf-rrsf") {
     const racf_rrsf_extract_results_t *p_rrsf_result =
         reinterpret_cast<const racf_rrsf_extract_results_t *>(p_raw_result);
-    raw_result_length = ntohl(p_rrsf_result->subpool_buffer_length);    
+    raw_result_length = ntohl(p_rrsf_result->subpool_buffer_length + 200);    
     Logger::getInstance().debug("Raw result length:");
     Logger::getInstance().debug(std::to_string(raw_result_length));
   } else if (request.getAdminType() == "racf-options") {
