e6f291668e3b25294b1ceef599e2c742fbb53b2c
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-11-11 15:56:09 +0100

    revert

--- sear/irrseq00/irrseq00.hpp
+++ sear/irrseq00/irrseq00.hpp
@@ -347,7 +347,7 @@ typedef struct {
   uint8_t automatic_redirection_application_updates;
   racf_rrsf_set_settings_t application_updates_redirection_settings[4];
   uint32_t number_of_rrsf_nodes;
-  char racf_subsystem_name[4 + 1];
+  char racf_subsystem_name[4];
   char racf_subsystem_userid[8];
   char reserved_space[52];
   racf_rrsf_node_definitions_t node_definitions;
