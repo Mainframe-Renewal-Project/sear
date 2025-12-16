1f6c8f80e0cae15f70091a6da40d32adf43cf2ce
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-12-16 03:02:11 +0100

    test

--- sear/irrseq00/irrseq00.hpp
+++ sear/irrseq00/irrseq00.hpp
@@ -276,7 +276,7 @@ const uint32_t RRSF_PRIVILEGED_ATTRIBUTE        = 0x00040000;
 
 typedef struct {
   uint32_t length;
-  char data[length];
+  char data[];
 } racf_rrsf_offset_field_t;
 
 typedef struct {
