1a248f685c532e2f4bcab5f55a713650b1205764
Emma Skovgård <64806882+EmmasBox@users.noreply.github.com>
2026-03-05 07:47:39 +0100

    Added authorization check for RRSF

--- sear/irrseq00/profile_post_processor.cpp
+++ sear/irrseq00/profile_post_processor.cpp
@@ -359,64 +359,69 @@ void ProfilePostProcessor::postProcessRACFRRSF(SecurityRequest &request) {
     profile["profile"]["base"]["base:nodes"] = nodes;
   }
 
-  if (ntohl(rrsf_extract_result->bit_flags) & RRSF_FULLRRSFCOMM_ACTIVE) {
-    profile["profile"]["base"]["base:full_rrsf_communication_active"] = true;
-  } else {
-    profile["profile"]["base"]["base:full_rrsf_communication_active"] = false;
-  }
+  // Checks if user is authorized to query RRSF settings
+  if (ntohl(rrsf_extract_result->bit_flags) ~ RRSF_NOT_AUTHORIZED_SET_LIST && ntohl(rrsf_extract_result->bit_flags) ~ RRSF_NOT_AUTHORIZED_TARGET_LIST) {
+    if (ntohl(rrsf_extract_result->bit_flags) & RRSF_FULLRRSFCOMM_ACTIVE) {
+      profile["profile"]["base"]["base:full_rrsf_communication_active"] = true;
+    } else {
+      profile["profile"]["base"]["base:full_rrsf_communication_active"] = false;
+    }
 
-  if (ntohl(rrsf_extract_result->bit_flags) & RRSF_SET_AUTODIRECT_ACTIVE) {
-    profile["profile"]["base"]["base:full_autodirect_active"] = true;
-  } else {
-    profile["profile"]["base"]["base:full_autodirect_active"] = false;
-  }
+    if (ntohl(rrsf_extract_result->bit_flags) & RRSF_SET_AUTODIRECT_ACTIVE) {
+      profile["profile"]["base"]["base:full_autodirect_active"] = true;
+    } else {
+      profile["profile"]["base"]["base:full_autodirect_active"] = false;
+    }
 
-  if (ntohl(rrsf_extract_result->bit_flags) & RRSF_SET_AUTODIRECT_APP_UPDATES) {
-    profile["profile"]["base"]["base:autodirect_application_updates"] = true;
-  } else {
-    profile["profile"]["base"]["base:autodirect_application_updates"] = false;
-  }
+    if (ntohl(rrsf_extract_result->bit_flags) & RRSF_SET_AUTODIRECT_APP_UPDATES) {
+      profile["profile"]["base"]["base:autodirect_application_updates"] = true;
+    } else {
+      profile["profile"]["base"]["base:autodirect_application_updates"] = false;
+    }
 
-  if (ntohl(rrsf_extract_result->bit_flags) & RRSF_SET_AUTO_PASSWORD_DIRECTION) {
-    profile["profile"]["base"]["base:autodirect_passwords"] = true;
-  } else {
-    profile["profile"]["base"]["base:autodirect_passwords"] = false;
-  }
+    if (ntohl(rrsf_extract_result->bit_flags) & RRSF_SET_AUTO_PASSWORD_DIRECTION) {
+      profile["profile"]["base"]["base:autodirect_passwords"] = true;
+    } else {
+      profile["profile"]["base"]["base:autodirect_passwords"] = false;
+    }
 
-  if (ntohl(rrsf_extract_result->bit_flags) & RRSF_SET_TRACE_APPC_ACTIVE) {
-    profile["profile"]["base"]["base:appc_trace_active"] = true;
-  } else {
-    profile["profile"]["base"]["base:appc_trace_active"] = false;
-  }
+    if (ntohl(rrsf_extract_result->bit_flags) & RRSF_SET_TRACE_APPC_ACTIVE) {
+      profile["profile"]["base"]["base:appc_trace_active"] = true;
+    } else {
+      profile["profile"]["base"]["base:appc_trace_active"] = false;
+    }
 
-  if (ntohl(rrsf_extract_result->bit_flags) & RRSF_SET_TRACE_IMAGE_ACTIVE) {
-    profile["profile"]["base"]["base:image_trace_active"] = true;
-  } else {
-    profile["profile"]["base"]["base:image_trace_active"] = false;
-  }
-  
-  if (ntohl(rrsf_extract_result->bit_flags) & RRSF_SET_TRACE_SSL_ACTIVE) {
-    profile["profile"]["base"]["base:ssl_trace_active"] = true;
-  } else {
-    profile["profile"]["base"]["base:ssl_trace_active"] = false;
-  }
+    if (ntohl(rrsf_extract_result->bit_flags) & RRSF_SET_TRACE_IMAGE_ACTIVE) {
+      profile["profile"]["base"]["base:image_trace_active"] = true;
+    } else {
+      profile["profile"]["base"]["base:image_trace_active"] = false;
+    }
+    
+    if (ntohl(rrsf_extract_result->bit_flags) & RRSF_SET_TRACE_SSL_ACTIVE) {
+      profile["profile"]["base"]["base:ssl_trace_active"] = true;
+    } else {
+      profile["profile"]["base"]["base:ssl_trace_active"] = false;
+    }
 
-  if (ntohl(rrsf_extract_result->bit_flags) & RRSF_PRIVILEGED_ATTRIBUTE) {
-    profile["profile"]["base"]["base:privileged_attribute_on"] = true;
-  } else {
-    profile["profile"]["base"]["base:privileged_attribute_on"] = false;
-  }
+    if (ntohl(rrsf_extract_result->bit_flags) & RRSF_PRIVILEGED_ATTRIBUTE) {
+      profile["profile"]["base"]["base:privileged_attribute_on"] = true;
+    } else {
+      profile["profile"]["base"]["base:privileged_attribute_on"] = false;
+    }
 
-  if (ntohl(rrsf_extract_result->bit_flags) & RRSF_TRUSTED_ATTRIBUTE) {
-    profile["profile"]["base"]["base:trusted_attribute_on"] = true;
-  } else {
-    profile["profile"]["base"]["base:trusted_attribute_on"] = false;
-  }
+    if (ntohl(rrsf_extract_result->bit_flags) & RRSF_TRUSTED_ATTRIBUTE) {
+      profile["profile"]["base"]["base:trusted_attribute_on"] = true;
+    } else {
+      profile["profile"]["base"]["base:trusted_attribute_on"] = false;
+    }
 
-  if (ntohl(rrsf_extract_result->bit_flags) & RRSF_NOT_ENOUGH_SPACE) {
-      request.setSEARReturnCode(4);
-      // Raise Exception if RRSF extract Failed.
-      throw SEARError("Not enough memory to extract RRSF settings");
+    if (ntohl(rrsf_extract_result->bit_flags) & RRSF_NOT_ENOUGH_SPACE) {
+        request.setSEARReturnCode(4);
+        // Raise Exception if RRSF extract Failed.
+        throw SEARError("Not enough memory to extract RRSF settings");
+    }
+  } else {
+    throw SEARError("Not authorized to query RRSF settings");
   }
   
   request.setIntermediateResultJSON(profile);
