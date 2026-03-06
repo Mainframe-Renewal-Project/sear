0e4ae3a357784e2c0c432562804ea08e2a8f590e
ZHAO Yinjie <zhaoyinjie1995@gmail.com>
2026-03-07 00:33:18 +0100

    replace xml generate by pugixml

--- sear/irrsmo00/xml_generator.hpp
+++ sear/irrsmo00/xml_generator.hpp
@@ -2,6 +2,7 @@
 #define __SEAR_XML_GENERATOR_H_
 
 #include <nlohmann/json.hpp>
+#include <pugixml.hpp>
 #include <string>
 
 #include "logger.hpp"
@@ -13,24 +14,24 @@ class XMLGenerator {
  private:
   std::string xml_string_;
   static std::string replaceXMLChars(std::string data);
-  void buildOpenTag(std::string tag);
-  void buildMetaTag();
   void buildAttribute(std::string name, std::string value);
-  void buildValue(std::string value);
-  void buildEndNestedTag();
-  void buildFullCloseTag(std::string tag);
-  void buildCloseTagNoValue();
-  void buildSingleTrait(const std::string& tag, const std::string& operation,
-                        const std::string& value);
   void buildXMLHeaderAttributes(const SEAR::SecurityRequest& request,
                                 const std::string& true_admin_type);
-  void buildRequestData(const std::string& true_admin_type,
-                        const std::string& admin_type,
-                        nlohmann::json request_data);
   static std::string convertOperation(const std::string& operation);
   static std::string convertOperator(const std::string& trait_operator);
   static std::string convertAdminType(const std::string& admin_type);
   std::string JSONValueToString(const nlohmann::json& trait);
+  void buildPugixmlHeaderAttributes(pugi::xml_node& node,
+                                    const SEAR::SecurityRequest& request,
+                                    const std::string& true_admin_type);
+  void buildPugixmlRequestData(pugi::xml_node& node,
+                               const std::string& true_admin_type,
+                               const std::string& admin_type,
+                               nlohmann::json request_data);
+  static void buildPugixmlSingleTrait(pugi::xml_node& node,
+                                      const std::string& tag,
+                                      const std::string& operation,
+                                      const std::string& value);
 
  public:
   void buildXMLString(SEAR::SecurityRequest& request);
